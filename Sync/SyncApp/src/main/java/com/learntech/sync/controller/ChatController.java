c1d0796affe8606198f3f92f55ac37e4cf092f9e
Vikas Gupta <vikaskumar2010@gmail.com>
2025-08-24 20:28:51 +0530

    Log integration SLF4J (#1)

--- Sync/SyncApp/src/main/java/com/learntech/sync/controller/ChatController.java
+++ Sync/SyncApp/src/main/java/com/learntech/sync/controller/ChatController.java
@@ -1,5 +1,7 @@
 package com.learntech.sync.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -11,12 +13,21 @@ import com.learntech.sync.service.IMessageHandlerService;
 @Controller
 public class ChatController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(ChatController.class);
+	
 	@Autowired
 	private IMessageHandlerService messgeHandlerService;
 	
 	@MessageMapping("/sendMessage") // client sends message here
     // @SendTo("/topic/messages")      // server broadcasts here
     public Message send(Message message) {
+		
+		// Logging test
+		logger.info("This is an info message");
+        logger.debug("This is a debug message");
+        logger.warn("This is a warning");
+        logger.error("This is an error message");
+		
 		System.out.println("Message received...");
 		this.messgeHandlerService.handleMessage(message);
         return message;
